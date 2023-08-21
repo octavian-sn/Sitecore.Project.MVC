2f245c3a1b8fb39bf16fb8da910fe7934ed6825c
Șulic Octavian <octavius1992@gmail.com>
2023-08-21 17:56:20 +0300

    Add items URL using LinkManager.

---
+++ Sitecore.Project.MVC.Web/Extensions/ItemExtension.cs
@@ -0,0 +1,24 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Links;
+using Sitecore.Links.UrlBuilders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Project.MVC.Web.Extensions
+{
+    public static class ItemExtension
+    {
+        public static string Url(this Item item, ItemUrlBuilderOptions options = null)
+        {
+            if (item == null)  
+                throw new ArgumentNullException(nameof(item)); 
+
+            if (options != null) 
+                return LinkManager.GetItemUrl(item, options);
+
+            return LinkManager.GetItemUrl(item);
+        }
+    }
+}
