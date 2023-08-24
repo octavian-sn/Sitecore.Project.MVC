a619b1ae9cc3304495d6afd09f3bd72b5afc888b
Șulic Octavian <octavius1992@gmail.com>
2023-08-24 17:52:28 +0300

    Add component rendering using data-source.

---
+++ Sitecore.Project.MVC.Web/Controllers/FeatureController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sitecore.Data.Fields;
+using Sitecore.Mvc.Presentation;
+using Sitecore.Project.MVC.Web.Models;
+using Sitecore.Web.UI.WebControls;
+
+namespace Sitecore.Project.MVC.Web.Controllers
+{
+    public class FeatureController : Controller
+    {
+        // GET: Feature
+        public ActionResult Index()
+        {
+            var dataSource = RenderingContext.Current?.Rendering.Item;
+            MultilistField featuresField = dataSource.Fields["feature"];
+
+            FeatureModel model = new FeatureModel();
+            List<Feature> features = new List<Feature>();
+
+            if(featuresField.Count > 0)
+            {
+                var featureItems = featuresField.GetItems();
+
+                foreach( var featureItem in featureItems )
+                {
+                    var title = new MvcHtmlString(FieldRenderer.Render
+                        (featureItem, "Title"));
+
+                    var icon = new MvcHtmlString(FieldRenderer.Render
+                        (featureItem, "Icon"));
+
+                    features.Add(new Feature
+                    {
+                        Title = title,
+                        Icon = icon,
+                    });
+                }
+
+                model.Features = features;
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Sitecore.Project.MVC.Web/Models/FeatureModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sitecore.Project.MVC.Web.Controllers;
+
+namespace Sitecore.Project.MVC.Web.Models
+{
+    public class FeatureModel
+    {
+        public List<Feature> Features { get; set; }
+    }
+
+    public class Feature
+    {
+        public MvcHtmlString Title { get; set; }
+        public MvcHtmlString Icon { get; set; }
+
+    }
+}
