aede97c563c4a180b3a1ba6331a221154be06719
Șulic Octavian <octavius1992@gmail.com>
2023-08-28 16:35:25 +0300

    Update rest nav items to use data source. Add freeQuote component using data source.

---
+++ Sitecore.Project.MVC.Web/Controllers/FreeQuoteController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sitecore.Mvc.Presentation;
+using Sitecore.Project.MVC.Web.Models;
+using Sitecore.Web.UI.WebControls;
+
+namespace Sitecore.Project.MVC.Web.Controllers
+{
+    public class FreeQuoteController : Controller
+    {
+        // GET: FreeQuote
+        public ActionResult Index()
+        {
+            var dataSource = RenderingContext.Current.Rendering.Item;
+
+            FreeQuoteModel model = new FreeQuoteModel
+            {
+                Title = new MvcHtmlString(FieldRenderer.Render(dataSource, "Title")),
+                Description = new MvcHtmlString(FieldRenderer.Render(dataSource, "Description")),
+                Image = new MvcHtmlString(FieldRenderer.Render(dataSource, "Image", "class=position-absolute img-fluid w-100 h-100"))
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ Sitecore.Project.MVC.Web/Models/FreeQuoteModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sitecore.Project.MVC.Web.Models
+{
+    public class FreeQuoteModel
+    {
+        public MvcHtmlString Title { get; set; }
+        public MvcHtmlString Description { get; set; }
+        public MvcHtmlString Image { get; set; }
+    }
+}
